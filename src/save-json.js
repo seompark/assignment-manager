eef19693018b8535ec849ab5ae16957dade28f85
MuRye <dreamaker7770@gmail.com>
2017-02-24 03:06:14 +0900

    Rename save.js to save-json.js

--- src/save-json.js
+++ src/save-json.js
@@ -1,16 +1,11 @@
 const sort = require('sort-json');
 const jsonfile = require('jsonfile');
 
-const pending = {};
-const busy = {};
-
 module.exports = (path, obj) => new Promise((resolve, reject) => {
-	busy[path] = obj;
 	obj = sort(obj);
 	jsonfile.writeFile(path, { spaces: 2 }, (err) => {
 		if(err)
 			return reject(err);
-		delete busy[obj];
 		resolve();
 	});
 });
